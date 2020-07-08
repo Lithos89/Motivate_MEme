1a0f8629e624eef22a37eaad4ebb84c1f14ae351
Lithos <edmogos89@gmail.com>
2020-07-07 19:43:17 -0600

    Initial Commit

---
+++ Motivate MEme/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Motivate MEme
+//
+//  Created by Eduard Mogos on 2020-07-07.
+//  Copyright © 2020 Eduard Mogos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
